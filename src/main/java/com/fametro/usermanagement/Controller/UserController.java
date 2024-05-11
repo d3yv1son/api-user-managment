4de2e4b083acf961b8718e9f17c632b7b664c391
EglanyJr <eglanyjr@gmail.com>
2024-05-11 15:17:05 -0400

    feat: Create user controller and entity

---
+++ src/main/java/com/fametro/usermanagement/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.fametro.usermanagement.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getRoot(){
+        return "server Running in port 8080";
+    }
+}
---
+++ src/main/java/com/fametro/usermanagement/entity/User.java
@@ -0,0 +1,56 @@
+package com.fametro.usermanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String password;
+    private String email;
+
+    public User() {}
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
