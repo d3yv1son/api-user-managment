a16808fbb48ea800c2dfbcfccf8d77c72ac59f08
laura-aguiar <beatrizlaura863@gmail.com>
2024-05-28 20:21:40 -0400

    feat: Create new entity for table Roles

---
+++ src/main/java/com/fametro/usermanagement/entity/Roles.java
@@ -0,0 +1,34 @@
+package com.fametro.usermanagement.entity;
+
+@Entity
+@TableName("roles")
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+
+    public Roles() {}
+
+    public Roles(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
