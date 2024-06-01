ed944649a478bf37d8d7a47ed3a7de0f443167d4
Eglany Junior <eglanyjr@gmail.com>
2024-06-01 12:26:43 -0400

    refactor: add new jwt library
    feat: create jwt service class and implements generate token method
    
    feat: create seeders to be project

--- src/main/java/com/fametro/usermanagement/Controller/LoginController.java
+++ src/main/java/com/fametro/usermanagement/Controller/LoginController.java
@@ -2,19 +2,33 @@ package com.fametro.usermanagement.Controller;
 
 import com.fametro.usermanagement.Controller.dto.LoginBodyDto;
 import com.fametro.usermanagement.Controller.dto.LoginResponseDto;
+import com.fametro.usermanagement.auth.JwtService;
+import com.fametro.usermanagement.entity.User;
+import com.fametro.usermanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public LoginResponseDto login(@RequestBody LoginBodyDto loginBody) {
-        System.out.println(loginBody.email() + loginBody.password());
-        String ficToken = "aleatoryToken";
+
+        Optional<User> user = userRepository.findById(Long.valueOf(1));
+
+        String token = jwtService.generateToken(user.orElse(null));
         String ficMessage = "User was been loged";
-        return LoginResponseDto.fromEntity(ficToken, ficMessage);
+        return LoginResponseDto.fromEntity(token, ficMessage);
     }
 }
