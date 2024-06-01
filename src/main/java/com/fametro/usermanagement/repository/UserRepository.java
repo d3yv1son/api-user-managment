14afdf80f08c300b6f33182ed50f9a10ddb76f7d
Eglany Junior <eglanyjr@gmail.com>
2024-06-01 12:57:31 -0400

    feat: create spring security configurations and implement authorization service

--- src/main/java/com/fametro/usermanagement/repository/UserRepository.java
+++ src/main/java/com/fametro/usermanagement/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.fametro.usermanagement.repository;
 
 import com.fametro.usermanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
 }
